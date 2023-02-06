4e149a1ffa7b68ded09998f96599df7e3e6ebd9a
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-02-06 15:09:19 +0100

    Proyecto inicial

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Hola mundo</Text>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
