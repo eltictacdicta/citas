e58a1d63fddb7a7e2fb6fc9e215b92e0380fa540
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-02-09 22:48:44 +0100

    Seguimos aplicando estilos

--- App.tsx
+++ App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {
   SafeAreaView,
   Text,
-  StyleSheet
+  StyleSheet,
+  Button
 } from 'react-native';
 
 function App(): JSX.Element {
   const nombre="Javier Trujillo"
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.titulo}>Administracion de citas{' '}
-      <Text>Veterinaria</Text>
+      <Text style={styles.tituloBold}>Veterinaria</Text>
       </Text>
+
+      <Button 
+      title='Nueva cita'
+      onPress={() => {
+         console.log('Presionastes el botón')
+      }
+
+      }
+      ></Button>
     </SafeAreaView>
   );
 
@@ -19,11 +29,20 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1
+  },
   titulo: {
     textAlign: 'center',
     color: '#374151',
     fontSize: 30,
     fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9'
+
   }
 })
 
