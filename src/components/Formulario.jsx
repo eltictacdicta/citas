67d99c395f5740781d5da0d7d1d1a8d8adc3fc26
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-02-13 15:05:41 +0100

    empezamos a darle estilo a el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,21 +2,47 @@ import React from 'react'
 import {
     Modal,
     Text,
-    SafeAreaView
+    SafeAreaView,
+    StyleSheet
 } from 'react-native'
 
 const Formulario = ({modalVisible}) => {
   return (
-    <SafeAreaView>
-      <Modal
+    <SafeAreaView 
+      style={styles.contenido}
+    >
+      <Modal 
         animationType='slide'
         visible={modalVisible}
       >
-         <Text>Desde Modal</Text>
+         <Text
+          style={styles.titulo}
+         >Nueva{' '}
+          <Text style={styles.tituloBold}>Cita</Text>
+         </Text>
       </Modal>
     </SafeAreaView>
     
   )
 }
+
+const styles = StyleSheet.create({
+  contenido:{
+    backgroundColor:'#000',
+    flex:1
+  },
+  titulo:{
+    fontSize:30,
+    fontWeight:'600',
+    textAlign:'center',
+    marginTop:30,
+    color: '#FFF'
+  },
+  tituloBold:{
+    fontWeight:'900'
+  }
+  
+})
+
 
 export default Formulario
